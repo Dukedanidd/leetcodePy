8dd26f6260b1861ba41c64dc92220c347578acab
Duke <jd.daniel.contact@gmail.com>
2025-01-19 19:05:31 -0600

    Excercise 4

---
+++ Excercises/Resolve/Easy/DuplicatesArray.py
@@ -0,0 +1,7 @@
+
+arreglo = [1, 1, 2, 3, 4, 4, 5, 6, 6, 7, 9]
+
+arreglo_sin_duplicados = list(set(arreglo))
+
+print('Arreglo con duplicados ' , arreglo)
+print('Arreglo sin duplicados ' , arreglo_sin_duplicados)

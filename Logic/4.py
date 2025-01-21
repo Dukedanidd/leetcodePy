a56d2f405b57c3849cd140e5613dfcfbe8be1b35
Duke <jd.daniel.contact@gmail.com>
2025-01-20 21:35:31 -0600

    Refactor FizzBuzz logic for improved readability

--- Logic/4.py
+++ Logic/4.py
@@ -5,9 +5,8 @@
         Si es divisible por 5, imprime "Buzz".
         Si es divisible por ambos, imprime "FizzBuzz".
 """
-
-for i in range(1,101):
-    if i % 3 == 0 and i % 5 == 0:
+for i in range(1, 101):
+    if i % 3 == 0  and i % 5 == 0:
         print('FizzBuzz')
     elif i % 3 == 0:
         print('Fizz')
